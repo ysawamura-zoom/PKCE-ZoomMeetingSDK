d7501ca664e9276a7226880792a72edb3d47a8c1
ysawamura-zoom <57646939+ysawamura-zoom@users.noreply.github.com>
2022-06-17 19:08:39 +0900

    Update Constants.java

--- app/src/main/java/com/sample/msdk/Constants.java
+++ app/src/main/java/com/sample/msdk/Constants.java
@@ -1,11 +1,11 @@
 package com.sample.msdk;
 
 public interface Constants {
-    String SDK_KEY = "vaCBSkY0HRpC1oMO86or4PJRdbfeZvflkpx1";
-    String SDK_SECRET = "mxEKr0vbl72pudINnWEszZ6uBQZ2q3HTT4XS";
-    String CLIENT_ID = "sqDfa1ScSRqMJAvYyeZmkQ";
-    String CLIENT_SECRET = "CR782524XK4dMyTOgl9CODFTOfsJvhj7";
-    String ADD_URL = "https://zoom.us/oauth/authorize?response_type=code&client_id=sqDfa1ScSRqMJAvYyeZmkQ&redirect_uri=https%3A%2F%2Fplayground.zapto.org%3A8443%2F";
-    String REDIRECT_DOMAIN = "playground.zapto.org";
-    String REDIRECT_URL_ENCODED = "https%3A%2F%2Fplayground.zapto.org%3A8443%2F";
+    String SDK_KEY = "<SDK Key from your Zoom Marketaplce SDK app entry>";
+    String SDK_SECRET = "<SDK Secret from your Zoom Marketaplce SDK app entry>";
+    String CLIENT_ID = "<Client ID from your Zoom Marketaplce SDK app entry>";
+    String CLIENT_SECRET = "Client Secret from your Zoom Marketaplce SDK app entry";
+    String ADD_URL = "<Add URL from Zoom your Marketplace SDK app entry>";
+    String REDIRECT_DOMAIN = "<Domain of your redirect url which was defined on your Zoom Marketaplce SDK app entry>";
+    String REDIRECT_URL_ENCODED = "<Encoded redirect url which was defined on your Zoom Marketaplce SDK app entry>";
 }
