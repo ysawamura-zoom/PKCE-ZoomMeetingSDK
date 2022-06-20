835fea2ec01b9ab3e2e4dc5cab75e0b66b06f5eb
ysawamura-zoom <57646939+ysawamura-zoom@users.noreply.github.com>
2022-06-20 12:02:07 +0900

    Update Constants.java

--- app/src/main/java/com/sample/msdk/Constants.java
+++ app/src/main/java/com/sample/msdk/Constants.java
@@ -7,5 +7,5 @@ public interface Constants {
     String CLIENT_SECRET = "Client Secret from your Zoom Marketaplce SDK app entry";
     String ADD_URL = "<Add URL from Zoom your Marketplace SDK app entry>";
     String REDIRECT_DOMAIN = "<Domain of your redirect url which was defined on your Zoom Marketaplce SDK app entry>";
-    String REDIRECT_URL_ENCODED = "<Encoded redirect url which was defined on your Zoom Marketaplce SDK app entry>";
+    String REDIRECT_URL_ENCODED = "<get your 'redirect url' which you defined on your Zoom Marketaplce SDK app entry and encoded and past it here>";
 }
